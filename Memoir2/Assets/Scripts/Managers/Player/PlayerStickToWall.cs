d22f0733d7671b70c6a4cabd4b8b9f10c4070779
Ethan <e_suter@outlook.com>
2026-04-06 17:26:07 -0400

    Sloppy programming fixed
    Moved stuck to wall stuff into it's own script.

---
+++ Memoir2/Assets/Scripts/Managers/Player/PlayerStickToWall.cs
@@ -0,0 +1,37 @@
+using StarterAssets;
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class PlayerStickToWall : MonoBehaviour
+{
+    ThirdPersonController movementScript;
+    private StarterAssetsInputs _input;
+    public bool IsOnWall = false;
+    private void Start()
+    {
+        movementScript = GetComponent<ThirdPersonController>();
+        _input = GetComponent<StarterAssetsInputs>();
+        Debug.Log(movementScript.Grounded);
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("PaintedWall"))
+        {
+            movementScript.Gravity = 0;
+            movementScript._verticalVelocity = 0;
+            Debug.Log("OnPaintedWall");
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PaintedWall"))
+        {
+            movementScript.Gravity = -15;
+            Debug.Log("ExitPaintedWall");
+
+
+        }
+    }
+} 
